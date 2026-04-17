ed4c336362f10b15502e3e6720df9a3694a5005d
maksimmalygin0879-beep <maksimmalygin0879@gmail.com>
2026-04-17 01:46:10 +0000

    fix: add missing state.dart import in tools.dart

--- lib/views/tools.dart
+++ lib/views/tools.dart
@@ -14,6 +14,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
 
 import 'theme.dart';
+import '../state.dart';
 
 class ToolsView extends ConsumerWidget {
   const ToolsView({super.key});
